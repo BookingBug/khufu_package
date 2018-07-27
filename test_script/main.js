c659527fa5a9c9da23fc368f1d1fc26e1f7fc599
Glenn <gshoosmith@gmail.com>
2018-07-27 11:16:57 +0100

    change function calls

--- test_script/main.js
+++ test_script/main.js
@@ -1,8 +1,8 @@
-exports.public_test = (event, context, callback) => {
+exports.public_test = (data, callback) => {
     // TODO implement
 
 
-    console.log("Public Test from Khufu", event, context)
+    console.log("Public Test from Khufu", data)
 
     callback(null, 'Hello from Lambda');
 
@@ -10,33 +10,33 @@ exports.public_test = (event, context, callback) => {
 };
 
 
-exports.admin_test = (event, context, callback) => {
+exports.admin_test = (data, callback) => {
     // TODO implement
 
 
-    console.log("Admin Test from Khufu", event, context)
+    console.log("Admin Test from Khufu", data)
 
     callback(null, 'Hello from Lambda');
 
 
 };
 
-exports.after_create_client_trigger = (client, callback) => {
+exports.after_create_client_trigger = (data, callback) => {
     // TODO implement
 
 
-    console.log("trying to create the client", event, context)
+    console.log("trying to create the client", data)
 
     callback(null, {success:false, connection_down: true});
 
 
 };
 
-exports.after_update_client_trigger = (client, callback) => {
+exports.after_update_client_trigger = (data, callback) => {
     // TODO implement
 
 
-    console.log("trying to create the client", event, context)
+    console.log("trying to create the client", data)
 
     callback(null, {success:true, connection_down: false});
 
