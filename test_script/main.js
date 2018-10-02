0bf21ba5fa0604dd3299f82800c2bd73843b5e50
Richard Kettle <richardlkettle@gmail.com>
2018-10-02 15:36:18 +0100

    further testing

--- test_script/main.js
+++ test_script/main.js
@@ -1,6 +1,9 @@
 exports.after_create_booking_trigger = (data, callback) => {
     // TODO implement
+    // Do some awesome stuff
     console.log("After creating the booking", data)
+    console.log("some logging", {a: "b"})
 
+    // to return errors from this script, return anything other than a 200
     callback(null, {richard: "Awesome", status: 303});
 };
