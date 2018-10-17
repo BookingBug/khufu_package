60a13ca5eb334c4b4334693d875f6458a0b03135
Gwe Limpalaer <glimpalaer@bookingbug.com>
2018-10-17 11:59:57 +0100

    [KHU-481] Update sso script

--- sso_script/main.js
+++ sso_script/main.js
@@ -2,7 +2,7 @@ exports.sso_script = (data, callback) => {
     console.log("Running SSO script with token:", data.token)
 
     if(data.token == 1234){
-        callback(null, {'status': 'success', 'data': {'first_name': 'Charles', 'last_name': 'Mumby', 'email': 'cmumby+1@bookingbug.com', 'mobile': '6175106267', 'member_id': '1'}});
+        callback(null, {'status': 'success', 'data': {'first_name': 'Gwe', 'last_name': 'Limpalaer', 'email': 'glimpalaer+10@bookingbug.com', 'mobile': '1231231233'}});
     }else{
         callback(null, {'status': 'failure', 'data': {'error': 'invalid token'}});
     }
